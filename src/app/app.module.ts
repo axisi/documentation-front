5b2c97a474ed5b6177e0357ad51f79d0a6f0a8c4
Grzegorz Macała <gmacala92@gmail.com>
2021-03-01 14:24:16 +0100

    1st commit - application created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContextComponent } from './context/context.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ContextComponent
   ],
   imports: [
     BrowserModule
